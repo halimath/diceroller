e107632f0dd47eab8e325d33f5397f446cb62104
cetiyeti <derceti@gmail.com>
2021-01-29 22:37:03 +0100

    fix tests

--- test/models/models.test.ts
+++ test/models/models.test.ts
@@ -1,6 +1,6 @@
-import {expect} from "chai"
+import { expect } from "chai"
 
-import {Die, DieResult, Pool, PoolResult, Symbol} from "../../src/models"
+import { Die, DieResult, Pool, PoolResult, DieSymbol } from "../../src/script/models"
 
 describe("Models", () => {
     describe("Die", () => {
@@ -26,71 +26,112 @@ describe("Models", () => {
         describe("normalize", () => {
             it("should normalized success and failure", () => {
                 const r = new PoolResult([
-                    new DieResult([Symbol.Success, Symbol.Success]),
-                    new DieResult([Symbol.Failure]),
+                    new DieResult([DieSymbol.Success, DieSymbol.Success]),
+                    new DieResult([DieSymbol.Failure]),
                 ])
 
                 expect(r.normalize()).to.deep.equal({
+                    "advantage": 0,
+                    "darkside": 0,
+                    "despair": 0,
+                    "failure": 0,
+                    "lightside": 0,
                     "success": 1,
+                    "threat": 0,
+                    "triumph": 0,
                 })
             })
 
             it("should normalized failure and success", () => {
                 const r = new PoolResult([
-                    new DieResult([Symbol.Failure, Symbol.Failure]),
-                    new DieResult([Symbol.Success]),
+                    new DieResult([DieSymbol.Failure, DieSymbol.Failure]),
+                    new DieResult([DieSymbol.Success]),
                 ])
 
                 expect(r.normalize()).to.deep.equal({
+                    "advantage": 0,
+                    "darkside": 0,
+                    "despair": 0,
                     "failure": 1,
+                    "lightside": 0,
+                    "success": 0,
+                    "threat": 0,
+                    "triumph": 0,
                 })
             })
 
             it("should normalized advantage and threat", () => {
                 const r = new PoolResult([
-                    new DieResult([Symbol.Advantage, Symbol.Advantage]),
-                    new DieResult([Symbol.Threat]),
+                    new DieResult([DieSymbol.Advantage, DieSymbol.Advantage]),
+                    new DieResult([DieSymbol.Threat]),
                 ])
 
                 expect(r.normalize()).to.deep.equal({
                     "advantage": 1,
+                    "darkside": 0,
+                    "despair": 0,
+                    "failure": 0,
+                    "lightside": 0,
+                    "success": 0,
+                    "threat": 0,
+                    "triumph": 0,
                 })
             })
 
             it("should normalized threat and advantage", () => {
                 const r = new PoolResult([
-                    new DieResult([Symbol.Threat, Symbol.Threat]),
-                    new DieResult([Symbol.Advantage]),
+                    new DieResult([DieSymbol.Threat, DieSymbol.Threat]),
+                    new DieResult([DieSymbol.Advantage]),
                 ])
 
                 expect(r.normalize()).to.deep.equal({
+                    "advantage": 0,
+                    "darkside": 0,
+                    "despair": 0,
+                    "failure": 0,
+                    "lightside": 0,
+                    "success": 0,
                     "threat": 1,
+                    "triumph": 0,
                 })
             })
 
             it("should normalized triumph", () => {
                 const r = new PoolResult([
-                    new DieResult([Symbol.Success, Symbol.Triumph]),
-                    new DieResult([Symbol.Failure]),
+                    new DieResult([DieSymbol.Success, DieSymbol.Triumph]),
+                    new DieResult([DieSymbol.Failure]),
                 ])
 
                 expect(r.normalize()).to.deep.equal({
+
+                    "advantage": 0,
+                    "darkside": 0,
+                    "despair": 0,
+                    "failure": 0,
+                    "lightside": 0,
                     "success": 1,
+                    "threat": 0,
                     "triumph": 1,
                 })
-            })            
+            })
 
             it("should normalized despair", () => {
                 const r = new PoolResult([
-                    new DieResult([Symbol.Failure, Symbol.Despair]),
-                    new DieResult([Symbol.Success]),
+                    new DieResult([DieSymbol.Failure, DieSymbol.Despair]),
+                    new DieResult([DieSymbol.Success]),
                 ])
 
                 expect(r.normalize()).to.deep.equal({
-                    "failure": 1,
+                    "advantage": 0,
+                    "darkside": 0,
                     "despair": 1,
+                    "failure": 1,
+                    "lightside": 0,
+                    "success": 0,
+                    "threat": 0,
+                    "triumph": 0,
                 })
-            })            
+            })
         })
     })
 })
